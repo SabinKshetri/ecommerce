12db30550715cadfe451c9da492b1ce06aed453f
sabin <shishirkshetri143@gmail.com>
2024-12-18 20:41:47 +0545

    changes

--- src/controller/addToCartController.ts
+++ src/controller/addToCartController.ts
@@ -1,4 +1,5 @@
 import Cart from "../database/models/cartModel";
+import Product from "../database/models/productModel";
 import { AuthRequest } from "../middleware/authMiddleware";
 import { Response } from "express";
 
@@ -31,6 +32,74 @@ class cartController {
     res.status(200).json({
       message: "Product added to cart",
       data: cartItem,
+    });
+  }
+  async getCartItem(req: AuthRequest, res: Response): Promise<void> {
+    const userId = req.user?.id;
+    const cartItems = await Cart.findAll({
+      where: {
+        userId,
+      },
+      include: [
+        {
+          model: Product,
+        },
+      ],
+    });
+    if (cartItems.length === 0) {
+      res.status(404).json({
+        message: "No Items in this cart",
+      });
+    } else {
+      res.status(200).json({
+        message: "Fetched cart items successfully !!",
+        data: cartItems,
+      });
+    }
+  }
+  async deleteMyCartItem(req: AuthRequest, res: Response): Promise<void> {
+    const userId = req.user?.id;
+    const { productId } = req.params;
+
+    //check whether above productid exist or not
+    const product = await Product.findByPk(productId);
+    if (!product) {
+      res.status(400).json({
+        message: "No product with this id",
+      });
+      return;
+    }
+    //delete that product from user cart
+    await Cart.destroy({
+      where: {
+        userId,
+        productId,
+      },
+    });
+    res.status(200).json({
+      message: "Items deleted successfully !",
+    });
+  }
+  async updateCartItem(req: AuthRequest, res: Response): Promise<void> {
+    const { productId } = req.params;
+    const userId = req.user?.id;
+    const { quantity } = req.body;
+    if (!quantity) {
+      res.status(400).json({
+        message: "please provide quantity",
+      });
+      return;
+    }
+    const cartData: any = await Cart.findOne({
+      where: {
+        userId,
+        productId,
+      },
+    });
+    cartData.quantity = quantity;
+    await cartData.save();
+    res.status(200).json({
+      message: "Cart item updated ",
     });
   }
 }
--- src/routes/cartRoutes.ts
+++ src/routes/cartRoutes.ts
@@ -1,10 +1,19 @@
 import express, { Router } from "express";
 import authMiddleware from "../middleware/authMiddleware";
 import cartController from "../controller/addToCartController";
+import errorHandler from "../services/catchAsyncError";
 const router: Router = express.Router();
 
 router
   .route("/")
-  .post(authMiddleware.isAuthenticated, cartController.addToCart);
+  .post(authMiddleware.isAuthenticated, errorHandler(cartController.addToCart))
+  .get(
+    authMiddleware.isAuthenticated,
+    errorHandler(cartController.getCartItem)
+  );
+router
+  .route("/:productId")
+  .patch(authMiddleware.isAuthenticated, cartController.updateCartItem)
+  .delete(authMiddleware.isAuthenticated, cartController.deleteMyCartItem);
 
 export default router;
